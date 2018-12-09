543ee3581c02f9a4fa8bad826b8a9322ffa088dd
Ramzey <rgnet1@gmail.com>
2018-12-08 18:40:04 -0800

    String bubble sorter

---
+++ sorting/basicArrayFuncs.c
@@ -0,0 +1,62 @@
+// Basic functions that are re-usable for arrays
+#include<string.h>
+#define MAX 100
+
+void swap(int *xp, int *yp) { 
+	int temp = *xp; 
+	*xp = *yp; 
+	*yp = temp; 
+} 
+
+//Denine len()
+#define len(array) (sizeof((array))/sizeof((array)[0]));
+
+// A function to implement bubble sort integers 
+void bubbleSortInt(int arr[], int n){ 
+	int i, j; 
+	for (i = 0; i < n-1; i++){ 
+		// Last i elements are already in place 
+		for (j = 0; j < n-i-1; j++){
+			if (arr[j] > arr[j+1])
+				swap(&arr[j], &arr[j+1]);
+		}
+	}
+} 
+
+//     bubbleSortString()
+//
+// A bubble sort algorithm to sort strings
+// 
+// @param: arr - Array of strings (MAX of 100 chars per string)
+// @param: int n - the size of the array
+void bubbleSortString(char arr[][MAX], int n){ 
+    char temp[MAX]; 
+    // Sorting strings using bubble sort 
+    for (int j=0; j<n-1; j++){ 
+        for (int i=j+1; i<n; i++){ 
+            if (strcmp(arr[j], arr[i]) > 0){ 
+                strcpy(temp, arr[j]); 
+                strcpy(arr[j], arr[i]); 
+                strcpy(arr[i], temp); 
+            }
+        }
+    }
+}
+
+
+//         insertion sort()
+
+/* Function to print an array */
+void printArrayInt(int arr[], int size){
+	int i;
+	for (i=0; i < size; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
+
+void printArrayString(char arr[][MAX], int size){
+	int i;
+	for (i=0; i < size; i++)
+		printf("%s ", arr[i]);
+	printf("\n");
+}
---
+++ sorting/bubble.c
@@ -0,0 +1,22 @@
+//      My Bubble sort example
+#include <stdio.h> 
+#include "basicArrayFuncs.c"
+
+// Driver program to test above functions 
+int main(){ 
+	int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+	int n = len(arr);
+	printf("len is %i\n", n);
+	bubbleSortInt(arr, n);
+	printf("Sorted ints with BubbleSort: \n");
+	printArrayInt(arr, n);
+
+	//MAX is 100, defined in basicArrayFuncs.c
+	char arrStr[][MAX] = {"Watch","Those","Wrist","Rockets","Please"}; 
+	n = len(arrStr);
+	printf("len is %i\n", n);
+	bubbleSortString(arrStr, n);
+	printf("Sorted String with BubbleSort: \n");
+	printArrayString(arrStr, n);
+	return 0;
+} 
